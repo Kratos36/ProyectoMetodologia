a3d5cbae101cc3777a047d8c5d046bf5121b1236
IndieBeto <beto.jeria@gmail.com>
2021-05-15 12:19:15 -0400

    Avances página estática.
    Aún no sabemos como poner css per vista. Por ahora usaremos el archivo css de la carpeta pública.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,40 @@
         <script src="{{ mix('js/app.js') }}"></script>
         <title>Document</title>
     </head>
-    <body>         
+    <body> 
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <img class="navbar-brand" src="{{ asset('/images/logo.png') }}" />
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07" aria-controls="navbarsExample07" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between" id="navbarsExample07">
+                    <ul class="navbar-nav flex-grow-1">
+                            <li class="nav-item active">
+                                <a class="nav-link">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown08" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Compra Ágil</a>
+                                <div class="dropdown-menu" aria-labelledby="dropdown08">
+                                    <a class="dropdown-item">Publicadas</a>
+                                    <a class="dropdown-item disabled">Adjudicadas</a>
+                                </div>
+                            </li>                        
+                        <li class="nav-item">
+                            <a class="nav-link">Bodega</a>
+                        </li>
+                    </ul>                
+
+                </div>
+            </div>
+        </nav>
+    </header> 
+    <div >
         @yield('content')
+    </div>
+      
+        
     </body>
 </html>
