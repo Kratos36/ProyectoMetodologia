26fa58f56a6e11fc0d8a9345b580efd4e8faf7c3
IndieBeto <beto.jeria@gmail.com>
2021-05-16 12:28:02 -0400

    Cambiar titulo pag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}" type="text/css">
         <script src="{{ mix('js/app.js') }}"></script>
-        <title>Document</title>
+        <title>NotiAPI</title>
     </head>
     <body> 
     <header>
